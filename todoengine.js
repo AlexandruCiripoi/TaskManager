8a6023f41240935d12ec894965769546204535f2
EKjs <doomedarchangel@gmail.com>
2021-06-16 12:25:22 +0200

    adding new tasks done

--- todoengine.js
+++ todoengine.js
@@ -1,6 +1,76 @@
 /* variables */
-let taskList={};
+let taskList={
+    ID1623744856709:{ /* ID can be generated using  generateId() */
+        caption:'some caption', /* text from text field */
+        description:'some description', /* text from text field */
+        dateStart:'2021-06-20', /* text from input type=date */
+        /* dates should be converted to toLocaleDateString() using new Date obj */
+        timeStart:'15:55', /* text from input type=time */
+        dateEnd:'2021-06-24', /* text from input type=date */
+        timeEnd:'15:55', /* text from input type=time */
+        category:1,/* a number that represents category (we can use an external obj for this list)
+        a category can be used for highlighting or to split tasks into groups */
+        currentState:0, /* a number that represents current state of the Task
+        0 - Todo
+        1 - in progress
+        2 - done */
+    },
+    ID1623744856218:{ /* ID can be generated using  generateId() */
+        caption:'1Create a GitHub repository', /* text from text field */
+        description:'And decide who does which part', /* text from text field */
+        dateStart:'2021-06-15', /* text from input type=date */
+        /* dates should be converted to toLocaleDateString() using new Date obj */
+        timeStart:'11:00', /* text from input type=time */
+        dateEnd:'2021-06-24', /* text from input type=date */
+        timeEnd:'15:55', /* text from input type=time */
+        category:1,/* a number that represents category (we can use an external obj for this list)
+        a category can be used for highlighting or to split tasks into groups */
+        currentState:1, /* a number that represents current state of the Task
+        0 - Todo
+        1 - in progress
+        2 - done */
+    },
+    ID1633744856218:{ /* ID can be generated using  generateId() */
+        caption:'2Create a GitHub repository', /* text from text field */
+        description:'And decide who does which part', /* text from text field */
+        dateStart:'2021-06-15', /* text from input type=date */
+        /* dates should be converted to toLocaleDateString() using new Date obj */
+        timeStart:'11:00', /* text from input type=time */
+        dateEnd:'2021-06-24', /* text from input type=date */
+        timeEnd:'15:55', /* text from input type=time */
+        category:1,/* a number that represents category (we can use an external obj for this list)
+        a category can be used for highlighting or to split tasks into groups */
+        currentState:2, /* a number that represents current state of the Task
+        0 - Todo
+        1 - in progress
+        2 - done */
+    },
+
+};
 let theStorage = window.localStorage;
+
+
+//fill up categories
+const categories = {
+    0:'No category',
+    1:'Learning',
+    2:'Eating',
+    3:'Exercises',
+    4:'Important',
+    5:'Not important',
+};
+const categoriesDropDown = document.getElementById('categoriesDropDown');
+for (let category in categories){
+    categoriesDropDown.innerHTML+=`<option value="${category}">${categories[category]}</option>`;
+}
+
+
+document.addEventListener('click',function(e){
+    if(e.target && e.target.dataset.btnType && e.target.dataset.taskId){
+        if (e.target.dataset.btnType==='removeTask')btnClickRemoveTask(e.target.dataset.taskId);
+        if (e.target.dataset.btnType==='editTask')btnClickEditTask(e.target.dataset.taskId);
+      }
+ });
 
 
 /* find the nodes to work with */
@@ -25,39 +95,68 @@ const doneDiv = document.getElementById('doneDiv');
 const btnSaveNewTask = document.getElementById('btnSaveNewTask');
 btnSaveNewTask.addEventListener('click',btnClickAddNewTask);
 
+const modalWindowAddNewTask = new bootstrap.Modal(document.getElementById('modalWindowAddNewTask'), {
+    keyboard: false
+  })
 
 
+// const btnReadFromLocalStorage = document.getElementById('btnReadFromLocalStorage');
+// btnReadFromLocalStorage.addEventListener('click',readFromLocalStorage);
 
-const btnReadFromLocalStorage = document.getElementById('btnReadFromLocalStorage');
-btnReadFromLocalStorage.addEventListener('click',readFromLocalStorage);
+// const btnListToConsole = document.getElementById('btnListToConsole');
+// btnListToConsole.addEventListener('click',listToConsole);
 
-const btnListToConsole = document.getElementById('btnListToConsole');
-btnListToConsole.addEventListener('click',listToConsole);
+// const btnListToLocalStorage = document.getElementById('btnListToLocalStorage');
+// btnListToLocalStorage.addEventListener('click',saveListToLocalStorage);
 
-const btnListToLocalStorage = document.getElementById('btnListToLocalStorage');
-btnListToLocalStorage.addEventListener('click',saveListToLocalStorage);
+
+taskList=readFromLocalStorage();
+showTaskList(taskList);
 
 
 /* Functions needed */
 
 function readFromLocalStorage() {
-    taskList = JSON.parse(theStorage.getItem('TaskList'));
-    console.log('List was read from local storage.');
-    showTaskList(taskList,outputField);
+    console.log('Reading list from local storage.');
+    const savedData = theStorage.getItem('TaskList'); 
+    if (!savedData){
+        return {}
+    }
+    return JSON.parse(savedData);
 }
 
-function saveListToLocalStorage() {
+function saveListToLocalStorage(taskList) {
     theStorage.setItem('TaskList',JSON.stringify(taskList));
     console.log('List was saved to local storage.')
 }
 
 
+/* listen to every click on the page and when we click on "special" buttons - call a function */
 document.addEventListener('click',function(e){
     if(e.target && e.target.dataset.btnType && e.target.dataset.taskId){
-        if (e.target.dataset.btnType==='removeTask')btnClickRemoveTask(e.target.dataset.taskId);
+        if (e.target.dataset.btnType==='taskToProgress')btnClickMoveTaskToProgress(e.target.dataset.taskId);
         if (e.target.dataset.btnType==='editTask')btnClickEditTask(e.target.dataset.taskId);
+        if (e.target.dataset.btnType==='taskBackToDo')btnClickMoveTaskBackToDo(e.target.dataset.taskId);
+        if (e.target.dataset.btnType==='taskToDone')btnClickMoveTaskToDone(e.target.dataset.taskId);
       }
  });
+
+function btnClickMoveTaskToProgress(taskID) {
+    taskList[taskID].currentState=1;
+    saveListToLocalStorage(taskList)
+    showTaskList(taskList);
+}
+
+function btnClickMoveTaskToDone(taskID) {
+    taskList[taskID].currentState=2;
+    saveListToLocalStorage(taskList)
+    showTaskList(taskList);
+}
+function btnClickMoveTaskBackToDo(taskID) {
+    taskList[taskID].currentState=0;
+    saveListToLocalStorage(taskList)
+    showTaskList(taskList);
+}
 
 function btnClickRemoveTask(taskID) {
     deleteTaskFromList(taskList,taskID);
@@ -73,10 +172,12 @@ function listToConsole() {
 }
 
 
-
+  
 function btnClickAddNewTask() {
     addNewTaskToTaskList(taskList);
-    showTaskList(taskList,outputField);
+    saveListToLocalStorage(taskList);
+    showTaskList(taskList);
+    modalWindowAddNewTask.toggle() //close moddal window
 }
 
 function addNewTaskToTaskList(taskList) {
@@ -87,36 +188,66 @@ function addNewTaskToTaskList(taskList) {
         timeStart:timeStartField.value,
         dateEnd:dateEndField.value,
         timeEnd:timeEndField.value,
-        category:0,
+        category:parseInt(categoriesDropDown.value),
         currentState:0,
     };
 }
 
-function showTaskList(taskList,whereToOutput) {
-    whereToOutput.innerHTML="<div>";
-    for (let task in taskList){
-        whereToOutput.innerHTML+=`<h2>${taskList[task].caption}</h2><p>${taskList[task].description}</p><h3>${taskList[task].timeStart} / ${taskList[task].dateStart}</h3><h3>${taskList[task].timeEnd} / ${taskList[task].dateEnd}</h3>`;
-        whereToOutput.innerHTML+=`<button data-btn-type='removeTask' data-task-id='${task}'>remove</button>`
-        whereToOutput.innerHTML+=`<button data-btn-type='editTask' data-task-id='${task}'>edit</button>`
-        // console.log(`Caption of ${task} is ${taskList[task].caption}`);
-        // console.log(`Description is ${taskList[task].description}`);
-        // console.log(`dateStart is ${taskList[task].dateStart}`);
-        // console.log(`dateEnd ${taskList[task].dateEnd}`);
+function showTaskList(taskList) {
+    let [state0,state1,state2] = splitByState(taskList);
+    toDoDiv.innerHTML="";
+    inProgressDiv.innerHTML="";
+    doneDiv.innerHTML="";
+    for (let task in state0){
+        toDoDiv.innerHTML+=prepareCard(task,state0[task]);
     }
-    whereToOutput.innerHTML+="</div>";
+    for (let task in state1){
+        inProgressDiv.innerHTML+=prepareCard(task,state1[task]);
+    }
+    for (let task in state2){
+        doneDiv.innerHTML+=prepareCard(task,state2[task]);
+    }
+
 }
 
-function prepareCard(taskData) {
-    const borderColor;
-    const taskCategory;
-    const taskCaption;
-    const taskDescription;
-    const taskTimeStart;
-    const taskDateStart;
-    const taskTimeEnd;
-    const taskDateEnd;
 
-    const cardTemplate = ```
+
+function categoryToText(category) {
+    return categories[category]
+}
+
+function prepareCard(taskID,taskData) {
+    //border color
+    console.log('taskData');
+    // console.log(taskData);
+    const borderColor=taskData.category===0 ? "dark" : taskData.category===1 ? "warning" : taskData.category===2 ? "info" : taskData.category===3 ? "success" : taskData.category===4 ? "danger" : "light";
+
+    const taskCategory=categoryToText(taskData.category);
+    const taskCaption=taskData.caption;
+    const taskDescription=taskData.description;
+    const taskTimeStart=taskData.timeStart;
+    const taskDateStart=taskData.dateStart;
+    const taskTimeEnd=taskData.timeEnd;
+    const taskDateEnd=taskData.dateEnd;
+
+    let addButtonsToCard = "";
+    if (taskData.currentState===0){
+        addButtonsToCard=`
+        <a href="#" class="btn btn-info border border-light" data-btn-type="editTask" data-task-id="${taskID}"> Edit </a>
+        <a href="#" class="btn btn-info border border-light" data-btn-type="taskToProgress" data-task-id="${taskID}"> In progress > </a>`;
+    }else if(taskData.currentState===1){
+        addButtonsToCard=`
+        <a href="#" class="btn btn-info border border-light" data-btn-type="taskBackToDo" data-task-id="${taskID}"> < ToDo </a>
+        <a href="#" class="btn btn-info border border-light" data-btn-type="editTask" data-task-id="${taskID}"> Edit </a>
+        <a href="#" class="btn btn-info border border-light" data-btn-type="taskToDone" data-task-id="${taskID}"> Done > </a>`;
+    }else if(taskData.currentState===2){
+        addButtonsToCard=`
+        <a href="#" class="btn btn-info border border-light" data-btn-type="taskToProgress" data-task-id="${taskID}"> < In progress </a>
+        <a href="#" class="btn btn-info border border-light" data-btn-type="editTask" data-task-id="${taskID}"> Edit </a>`;
+    };
+
+
+    const cardTemplate = `
     <!-- CARDS -->
     <!-- Card begin (row) -->
     <div class="row m-1">
@@ -157,9 +288,7 @@ function prepareCard(taskData) {
               <!-- we can add <hr> here -->
               <div class="col">
                 <div class="btn-group" role="group" aria-label="Basic example">
-                  <a href="#" class="btn btn-info border border-light">< ToDo </a>
-                  <a href="#" class="btn btn-info border border-light"> Edit </a>
-                  <a href="#" class="btn btn-info border border-light"> Done! > </a>
+                ${addButtonsToCard}
                 </div>
               </div>
             </div>
@@ -173,7 +302,10 @@ function prepareCard(taskData) {
 
 
       <!-- END CARDS -->
-    ```;
+    `;
+
+
+    return cardTemplate
 }
 
 
@@ -192,9 +324,9 @@ Usage:
 let [state0,state1,state2] = splitByState(taskList);
 */
 function splitByState(taskList) { 
-    let state0;
-    let state1;
-    let state2;
+    let state0={};
+    let state1={};
+    let state2={};
     for (let task in taskList){
         if (taskList[task].currentState===0)state0[task]=taskList[task];
         if (taskList[task].currentState===1)state1[task]=taskList[task];
